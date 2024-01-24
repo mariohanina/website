12524231c0e9a78b86a2d69fc5235c283e058c58
mariohanina <marionolastname21@gmail.com>
2024-01-23 22:19:48 -0800

    stock app progress

--- public/scripts/stocks.js
+++ public/scripts/stocks.js
@@ -1,11 +1,11 @@
 const baseUrl = "http://localhost:3000/";
 
-const input = document.querySelector("#searched-company");
-const searchBtn = document.querySelector("#search");
+const input = document.querySelector("#search-input");
+const searchBtn = document.querySelector("#search-button");
 const searchOutcome = document.querySelector("#search-outcome");
 const modal = document.querySelector("#modal");
 const close = document.querySelector("#close");
-const getCoDataBtn = document.querySelector("#get-co-data");
+const getDataBtn = document.querySelector("#get-data");
 
 let listOfCompanies = [];
 
@@ -34,6 +34,68 @@ function radioFinder(radios) {
     return chosenRadio;
 }
 
+// Finds which companies match the user's search phrases
+function matchingCompanies() {
+    modal.style.display = "flex";
+    let cleanedInput = input.value.trim();
+
+    if (cleanedInput.length > 0) {
+        // ADD A LOADING SCREEN
+        if (listOfCompanies.length < 1) {
+            getRequest("options").then(function (result) {
+                listOfCompanies = result.options;
+                displayAvailableCompanies();
+            });
+        } else {
+            displayAvailableCompanies();
+        }
+    } else {
+        searchOutcome.innerHTML = `<span class="large-text">You didn't type anything!</span>`;
+    }
+}
+
+// Displays what companies are available, if none are, it displays a message informing the user
+function displayAvailableCompanies() {
+
+    let matches = "";
+
+    // If the the input starts with "#", then we search by ticker symbol, otherwise we search by name
+    if (input.value[0] === "#") {
+        listOfCompanies.forEach(element => {
+            let companyTickerSymbol = "#" + element[0];
+            if (companyTickerSymbol.toLowerCase() === input.value.toLowerCase()) {
+                matches += `<label class="available-choices-label" for="${element[0]}">
+                    <input class="available-choices" type="radio" id="${element[0]}" 
+                        name="chosen-company"> ${element[1]}</label>`
+            }
+        });
+    } else {
+        listOfCompanies.forEach(element => {
+            if (element[1].toLowerCase().includes(input.value.toLowerCase())) {
+                matches += `<label class="available-choices-label" for="${element[0]}">
+                <input class="available-choices" type="radio" id="${element[0]}" 
+                    name="chosen-company"> ${element[1]} <strong>(${element[0]})</strong></label>`
+            }
+        });
+    }
+
+    // If no matches were found we display this screen
+    if (matches === "") {
+        matches = '<span class="large-text">Sorry, no results found.</span>';
+    } else {
+        getDataBtn.style.display = "block";
+    }
+
+    searchOutcome.innerHTML = matches;
+}
+
+function cleanUp() {
+    input.value = "";
+    searchOutcome.innerHTML = "";
+    getDataBtn.style.display = "none";
+    modal.style.display = "none";
+}
+
 // Updates the chart
 // async function updateChartData(theChart, theLabel, theData) {
 //     theChart.data.labels = theLabel;
@@ -43,79 +105,5 @@ function radioFinder(radios) {
 
 
 
-
-
-
-// Finds which companies match the user's search phrases
-function matchingCompanies() {
-
-    let cleanedInput = input.value.trim()
-    modal.style.display = "flex";
-
-    if (cleanedInput.length > 0) {
-        // Creates a loading screen
-        // matchingCompanies.html(loading);
-        getCoDataBtn.style.display = "block";
-
-        if (listOfCompanies.length < 1) {
-            getRequest("options").then(function (result) {
-                listOfCompanies = result.options;
-                // displayAvailableCompanies();
-            });
-        } else {
-            // displayAvailableCompanies();
-        }
-    } else {
-        searchOutcome.innerHTML = `<span class="large-text">No input was provided!</span>`;
-        getCoDataBtn.style.display = "none";
-    }
-
-}
-
-
-
-searchBtn.addEventListener("click", matchingCompanies)
-close.addEventListener("click", () => { modal.style.display = "none"; })
-
-
-
-
-
-
-
-
-
-// Displays what companies are available, if none are, it displays a message informing the user
-// function displayAvailableCompanies() {
-//     let matches = "";
-
-//     // If the the input starts with "#", then we search by ticker symbol, otherwise we search by name
-//     if (input.value[0] === "#") {
-//         listOfCompanies.forEach(element => {
-//             let companyTickerSymbol = "#" + element[0];
-//             if (companyTickerSymbol.toLowerCase() === input.value.toLowerCase()) {
-//                 matches += `<input type="radio" class="available-choices" id="${element[0]}" name="chosen-company">
-//                     <label class="available-choices-label" for="${element[0]}">${element[1]}</label>
-//                     <br>`
-//             }
-//         });
-//     } else {
-//         listOfCompanies.forEach(element => {
-//             if (element[1].toLowerCase().includes(input.value.toLowerCase())) {
-//                 matches += `<input type="radio" class="available-choices" id="${element[0]}" name="chosen-company">
-//                     <label class="available-choices-label" for="${element[0]}">${element[1]} <strong>(${element[0]})</strong></label>
-//                     <br>`
-//             }
-//         });
-//     }
-
-//     // If no matches were found we display this screen
-//     if (matches === "") {
-//         matches = '<span class="fs-4">Sorry, no results found.</span>';
-//     } else {
-//         getData.css("display", "block");
-//     }
-
-//     // searchModal.css("display", "block");
-//     matchingCompanies.html(matches);
-// }
+searchBtn.addEventListener("click", matchingCompanies);
+close.addEventListener("click", () => cleanUp());
