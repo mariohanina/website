2a6ba2bb8fecba7e64c98ffc25c40c0fb7e873a6
mariohanina <marionolastname21@gmail.com>
2024-03-04 09:58:47 -0800

    redesign image-viewer

--- public/scripts/image-viewer.js
+++ public/scripts/image-viewer.js
@@ -1,21 +1,31 @@
-// Code to display a modal describing the controls for the image viewer
-const closeBtn = document.querySelector("#stop-showing-btn");
-const checkBox = document.querySelector("#stop-showing");
-const modal = document.querySelector("#modal");
+// Html elements
+const modalClose = document.querySelector("#modal-close");
+const stopShowingBtn = document.querySelector("#stop-showing");
+const image = document.querySelector("#image");
+const scrollDiv = document.querySelector("#scrollable-container");
 const previousImage = document.querySelector("#prev-img");
 const nextImage = document.querySelector("#next-img");
-const imageDisplayed = document.querySelector("#image-displayed");
-const closeButtonLink = document.querySelector("#close-button-link");
+const closeBtn = document.querySelector("#close-btn");
 
-closeBtn.addEventListener("click", () => {
-    if (checkBox.checked) localStorage.setItem("first-time", "true");
+// Variable declarations
+let clicking = false;
+let touching = false;
+let previousX;
+let previousY;
+let maxDimension = 100;
+let mainDim;
+let npcDim;
+
+modalClose.addEventListener("click", () => {
+    if (stopShowingBtn.checked) localStorage.setItem("first-time", "true");
     modal.style.display = "none";
 });
 
 if (!localStorage.getItem("first-time")) modal.style.display = "flex";
 
-imageDisplayed.src = `/images/${currentFolder}/${currentImage}`
-closeButtonLink.href = `/${(currentFolder == "drawings") ? "art-gallery" : "photo-gallery"}`
+image.src = `/images/${currentFolder}/${currentImage}`
+// The answer lies in history.back()
+closeBtn.href = `/${(currentFolder == "drawings") ? "art-gallery" : "photo-gallery"}`
 
 const imageList = imageListString.split(",");
 const currentImageIndex = imageList.indexOf(currentImage);
@@ -31,221 +41,96 @@ if (currentImageIndex === 0) {
     nextImage.href = `/image-viewer/${currentFolder}/${imageList[currentImageIndex + 1]}`
 }
 
-// Original Code by Kwdowik: https://github.com/kwdowik/zoom-pan
-const hasPositionChanged = ({ pos, prevPos }) => pos !== prevPos;
+// If image is taller than window, height is the main dimensions.
+// If image is wider than window, width is the main dimension.
+function findDims() {
+    const { innerWidth, innerHeight } = window;
+    const isHeightLarger = ((innerWidth / innerHeight)
+        > (image.naturalWidth / image.naturalHeight));
 
-const valueInRange = ({ minScale, maxScale, scale }) => scale <= maxScale && scale >= minScale;
+    mainDim = isHeightLarger ? "height" : "width";
+    npcDim = isHeightLarger ? "width" : "height";
 
-const getTranslate = ({ minScale, maxScale, scale }) => ({ pos, prevPos, translate }) =>
-    valueInRange({ minScale, maxScale, scale }) && hasPositionChanged({ pos, prevPos })
-        ? translate + (pos - prevPos * scale) * (1 - 1 / scale)
-        : translate;
-
-const getScale = ({ scale, minScale, maxScale, scaleSensitivity, deltaScale }) => {
-    let newScale = scale + (deltaScale / (scaleSensitivity / scale));
-    newScale = Math.max(minScale, Math.min(newScale, maxScale));
-    return [scale, newScale];
-};
-
-const getMatrix = ({ scale, translateX, translateY }) => `matrix(${scale}, 0, 0, ${scale}, ${translateX}, ${translateY})`;
-
-const pan = ({ state, originX, originY }) => {
-    state.transformation.translateX += originX;
-    state.transformation.translateY += originY;
-    state.element.style.transform =
-        getMatrix({ scale: state.transformation.scale, translateX: state.transformation.translateX, translateY: state.transformation.translateY });
-};
-
-const MakeInfo = (state) => ({
-    zoomInfo: () => (state.transformation.scale),
-    locationInfo: () => ({
-        translateX: state.transformation.translateX,
-        translateY: state.transformation.translateY
-    })
-});
-
-const makePan = (state) => ({
-    panBy: ({ originX, originY }) => pan({ state, originX, originY }),
-    panTo: ({ originX, originY, scale }) => {
-        state.transformation.scale = scale;
-        pan({ state, originX: originX - state.transformation.translateX, originY: originY - state.transformation.translateY });
-    },
-});
-
-const makeZoom = (state) => ({
-    zoom: ({ x, y, deltaScale }) => {
-        const { left, top } = state.element.getBoundingClientRect();
-        const { minScale, maxScale, scaleSensitivity } = state;
-        const [scale, newScale] = getScale({ scale: state.transformation.scale, deltaScale, minScale, maxScale, scaleSensitivity });
-        const originX = x - left;
-        const originY = y - top;
-        const newOriginX = originX / scale;
-        const newOriginY = originY / scale;
-        const translate = getTranslate({ scale, minScale, maxScale });
-        const translateX = translate({ pos: originX, prevPos: state.transformation.originX, translate: state.transformation.translateX });
-        const translateY = translate({ pos: originY, prevPos: state.transformation.originY, translate: state.transformation.translateY });
-
-        state.element.style.transformOrigin = `${newOriginX}px ${newOriginY}px`;
-        state.element.style.transform = getMatrix({ scale: newScale, translateX, translateY });
-        state.transformation = { originX: newOriginX, originY: newOriginY, translateX, translateY, scale: newScale };
-    }
-});
-
-const renderer = ({ minScale, maxScale, element, scaleSensitivity = 10 }) => {
-    const state = {
-        element,
-        minScale,
-        maxScale,
-        scaleSensitivity,
-        transformation: {
-            originX: 0,
-            originY: 0,
-            translateX: 0,
-            translateY: 0,
-            scale: 1
-        },
-    };
-    return Object.assign({}, makeZoom(state), makePan(state), MakeInfo(state));
-};
-
-
-// Start up the app
-const container = document.getElementById("image-container");
-const instance = renderer({ minScale: 1, maxScale: 5, element: container.children[0], scaleSensitivity: 30 });
-
-
-// Zooming in by buttons
-document.querySelector("#zoom-in").addEventListener("click", () => {
-    instance.zoom({
-        deltaScale: 5,
-        x: window.innerWidth / 2,
-        y: window.innerHeight / 2
-    });
-});
-// Zooming out by buttons
-document.querySelector("#zoom-out").addEventListener("click", () => {
-    instance.zoom({
-        deltaScale: -5,
-        x: window.innerWidth / 2,
-        y: window.innerHeight / 2
-    });
-});
-// Zooming in/out by scroll wheel
-container.addEventListener("wheel", (event) => {
-    event.preventDefault();
-    instance.zoom({
-        deltaScale: Math.sign(event.deltaY) > 0 ? -1 : 1,
-        x: event.pageX,
-        y: event.pageY
-    });
-});
-
-
-// Restore default state function
-function restoreDefaultState(event) {
-    if (instance.zoomInfo() !== 1 ||
-        instance.locationInfo().translateX !== 0 ||
-        instance.locationInfo().translateY !== 0) {
-        instance.panTo({
-            originX: 0,
-            originY: 0,
-            scale: 1,
-        });
-    } else {
-        instance.zoom({
-            deltaScale: 50,
-            x: event.pageX,
-            y: event.pageY
-        });
-    }
+    zoom(innerWidth / 2, innerHeight / 2, 1);
 }
-// Restore default state by button
-document.querySelector("#zoom-original").addEventListener("click", () => {
-    instance.panTo({
-        originX: 0,
-        originY: 0,
-        scale: 1,
-    });
-});
-// Restore default state by double clicking
-container.addEventListener("dblclick", (event) => { restoreDefaultState(event) });
-// Restore default state by double tapping
-// let mylatesttap;
-// function doubletap(event) {
-//     let now = new Date().getTime();
-//     let timesince = now - mylatesttap;
-//     if ((timesince < 400) && (timesince > 0)) { restoreDefaultState(event) }
 
-//     mylatesttap = new Date().getTime();
-// }
-// container.addEventListener("touchstart", (e) => {
-//     const touch = e.touches[0];
-//     doubletap(touch);
-// });
+// Zoom and then rescroll
+const zoom = (x, y, zoomAmount) => {
+    // Save the current scroll location of the image
+    const { scrollLeft, scrollTop } = scrollDiv;
 
+    // Make sure zoom is within limits
+    const currentZoom = maxDimension;
+    const newZoom = Math.min(1000, Math.max(currentZoom * zoomAmount, 100));
+    const zoomRatio = newZoom / currentZoom;
+    maxDimension = newZoom;
 
-// Panning by mouse
-container.addEventListener("mousemove", (event) => {
-    if (!(event.buttons & 1 || (event.buttons === undefined && event.which == 1))) {
-        return;
+    // Apply zoom to html element
+    image.style.maxWidth = `${maxDimension}vw`;
+    image.style.maxHeight = `${maxDimension}vh`;
+    image.style[mainDim] = `${maxDimension}v${mainDim[0]}`;
+    image.style[npcDim] = `auto`;
+
+    // Scroll the image
+    scrollDiv.scrollLeft = ((scrollLeft + x) * zoomRatio) - x;
+    scrollDiv.scrollTop = ((scrollTop + y) * zoomRatio) - y;
+}
+
+// Recalculate the main and npc dimensions when images loads or screen resizes
+image.onload = findDims;
+window.addEventListener("resize", findDims);
+
+// Add the zoom functionality to wheel events
+document.addEventListener("wheel", (e) => {
+    if (e.shiftKey || e.deltaY > 0 || e.deltaY < -50) {
+        zoom(e.clientX, e.clientY, Math.sign(e.deltaY) < 0 ? 1.05 : 1 / 1.05,)
     }
+});
 
-    event.preventDefault();
-    instance.panBy({
-        originX: event.movementX,
-        originY: event.movementY
-    });
-})
-// Panning by touching
-// let previousTouch;
-// container.addEventListener('touchmove', (e) => {
-//     const touch = e.touches[0];
+// Gradually zoom out when user doubleclicks
+image.addEventListener("dblclick", (e) => {
+    for (let index = 3; index < (50 * 3); index += 3) {
+        setTimeout(() => {
+            zoom(e.clientX, e.clientY, 1 / 1.05)
+        }, index)
+    }
+});
 
-//     if (previousTouch && e.targetTouches.length == 1) {
-//         console.log("one fing: " + e.targetTouches.length);
-//         e.movementX = touch.pageX - previousTouch.pageX;
-//         e.movementY = touch.pageY - previousTouch.pageY;
+// THE PANNING FUNCTIONALITY
+scrollDiv.addEventListener("pointerdown", (e) => {
+    if (!(e.pointerType === "mouse")) {
+        return
+    }
+    e.preventDefault();
+    previousX = e.clientX;
+    previousY = e.clientY;
+    clicking = true;
+});
 
-//         e.preventDefault();
-//         instance.panBy({
-//             originX: e.movementX,
-//             originY: e.movementY
-//         });
-//     };
+document.addEventListener("pointerup", (e) => {
+    if (!(e.pointerType === "mouse")) {
+        return
+    }
+    clicking = false;
+});
 
-//     previousTouch = touch;
-// }, false);
-// container.addEventListener("touchend", (e) => {
-//     previousTouch = null;
-// });
+scrollDiv.addEventListener("pointerleave", (e) => {
+    if (!(e.pointerType === "mouse")) {
+        return
+    }
+    clicking = false;
+});
 
-
-// RISKY CODE
-// let firstPreviousTouch;
-// let secondPreviousTouch;
-// container.addEventListener('touchmove', (e) => {
-
-//     if (e.targetTouches.length == 2) {
-//         console.log("hehe");
-//         const firstFinger = e.touches[0];
-//         const secondFinger = e.touches[1];
-
-//         let dist = Math.hypot(
-//             firstFinger.pageX - secondFinger.pageX,
-//             firstFinger.pageY - secondFinger.pageY);
-//         let previousDist = Math.hypot(
-//             firstPreviousTouch.pageX - secondPreviousTouch.pageX,
-//             firstPreviousTouch.pageY - secondPreviousTouch.pageY);
-//         let finalDist = dist - previousDist;
-
-//         e.preventDefault();
-
-//         instance.zoom({
-//             deltaScale: Math.sign(finalDist) > 0 ? -1 : 1,
-//             x: window.innerWidth / 2,
-//             y: window.innerHeight / 2
-//         });
-//         firstPreviousTouch = firstFinger;
-//         secondPreviousTouch = secondFinger;
-//     };
-// }, false);
+// Pan the image by scrolling
+scrollDiv.addEventListener("pointermove", (e) => {
+    if (!(e.pointerType === "mouse")) {
+        return
+    }
+    if (clicking) {
+        e.preventDefault();
+        scrollDiv.scrollLeft = scrollDiv.scrollLeft + (previousX - e.clientX);
+        scrollDiv.scrollTop = scrollDiv.scrollTop + (previousY - e.clientY);
+        previousX = e.clientX;
+        previousY = e.clientY;
+    }
+});
