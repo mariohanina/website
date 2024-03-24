d11c4e586db1823d41f7f5b67bb0ffee23ae0e78
mariohanina <127279434+mariohanina@users.noreply.github.com>
2024-03-24 13:57:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-module.exports.email = "hsyfbnatql376@gmail.com"
-module.exports.password = "ivzxgasqhtotamri"
-module.exports.to = "mariogamer329@gmail.com"
 
-// module.exports.apiKey = "T9HTJL0BTNMVDTYM"
